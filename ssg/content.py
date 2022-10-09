86033136468575f77b964979e46603c3fb293d45
Narek Sahakyan <sahNarek>
2022-10-09 18:48:23 +0400

    module 3 fix 5

--- ssg/content.py
+++ ssg/content.py
@@ -26,8 +26,8 @@ class Content(Mapping):
         return self.data["type"] if "type" in self.data else None
     
     @type.setter
-    def type(self, property):
-        self.data["type"] = property
+    def type(self, type):
+        self.data["type"] = type
 
     def __getitem__(self, key):
         return self.data[key]
